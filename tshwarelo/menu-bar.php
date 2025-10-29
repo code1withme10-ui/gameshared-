d60a7bec8828f6253350a0c198f40c055369c462
Gajeni44 <tshwarelon8@gmail.com>
2025-10-29 13:51:05 +0200

    Feature: Finalized kids.php page and confirmed link in menu-bar.php is working.

--- tshwarelo/menu-bar.php
+++ tshwarelo/menu-bar.php
@@ -3,10 +3,12 @@
         <nav>
             <a href="index.php">Home</a> |
             <a href="about.php">About</a> |
+            <a href="staff.php">staff</a> |
             <a href="registration.php">Registration</a> |
             <a href="gallery.php">Gallery</a> |
-            <a href="code-of-conduct.php">Code of Conduct</a>
-            <a href="staff.php">staff</a>
+            <a href="code-of-conduct.php">Code of Conduct</a> |
+            <a href="kids.php">Kids' Names</a> |
+            
         </nav>
     </header>
     <h1>Welcome to Happy Kids Creche</h1>
