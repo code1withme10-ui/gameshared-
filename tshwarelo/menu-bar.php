fba966b470f0fdedbe8a5d06c49ae84cb3011cce
Gajeni44 <tshwarelon8@gmail.com>
2025-10-30 09:39:27 +0200

    feat: Updated navigation links and added new page to menu-bar.php

--- tshwarelo/menu-bar.php
+++ tshwarelo/menu-bar.php
@@ -3,7 +3,7 @@
         <nav>
             <a href="index.php">Home</a> |
             <a href="about.php">About</a> |
-            <a href="staff.php">staff</a> |
+            <a href="staff.php">Staff</a> |
             <a href="registration.php">Registration</a> |
             <a href="gallery.php">Gallery</a> |
             <a href="code-of-conduct.php">Code of Conduct</a> |
