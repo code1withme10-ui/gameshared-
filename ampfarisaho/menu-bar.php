3a24065a3135aab0239d7f1175d4aad143b9b803
ampfarisaho <mathaguampfarisaho@gmail.com>
2025-11-05 20:12:57 +0200

    Added the help page

--- ampfarisaho/menu-bar.php
+++ ampfarisaho/menu-bar.php
@@ -6,7 +6,8 @@
             <a href="registration.php">Registration</a> |
             <a href="gallery.php">Gallery</a> |
             <a href="staff.php">Staff</a> |
-            <a href="code-of-conduct.php">Code of Conduct</a>
+            <a href="code-of-conduct.php">Code of Conduct</a> |
+            <a href="help.php">Help</a> |
             <a href="login.php">Login</a>
         </nav>
     </header>
