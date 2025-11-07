db07bf91f49ebcea641cd775c952ea2dcd9282ea
ampfarisaho <mathaguampfarisaho@gmail.com>
2025-11-07 10:33:40 +0200

    Modified gallery page

--- ampfarisaho/gallery.php
+++ ampfarisaho/gallery.php
@@ -1,9 +1,10 @@
 <?php
 $content = '
   <h2>Our Images</h2>
-  <img src="https://via.placeholder.com/200" alt="Kids Playing">
-  <img src="https://via.placeholder.com/200" alt="Activities">
-  <img src="https://via.placeholder.com/200" alt="Learning Time">
+      <img src="https://images.pexels.com/photos/1181345/pexels-photo-1181345.jpeg" alt="Team working on project">
+      <img src="https://images.pexels.com/photos/1181396/pexels-photo-1181396.jpeg" alt="Team collaborating">
+      <img src="https://images.pexels.com/photos/1181671/pexels-photo-1181671.jpeg" alt="Team coding">
+      <img src="https://images.pexels.com/photos/1181346/pexels-photo-1181346.jpeg" alt="Team brainstorming">
 ';
 include 'layout.php';
 ?>
