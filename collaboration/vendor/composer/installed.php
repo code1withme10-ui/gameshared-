60ce32450e4a8aa857f0796e2a8a87dd0cf6d168
Takalani <takalanimusubi117@gmail.com>
2026-02-16 10:25:36 +0200

    Ignore vendor and cache folders

---
+++ collaboration/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '8dcccbfdc93e37b48d644e0a8c82a38cd169f833',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '8dcccbfdc93e37b48d644e0a8c82a38cd169f833',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
